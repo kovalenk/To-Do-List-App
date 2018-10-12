f48e9e2c3cd5e8a1f182568df7dceeae930b4875
kovalenk <yura.kovalenko.1996.29@gmail.com>
2018-10-12 12:57:54 +0300

    Full start page with adaptive design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,13 @@ import {AngularFireDatabaseModule} from 'angularfire2/database';
 import {AppComponent} from './app.component';
 import {environment} from '../environments/environment';
 import {TodoComponent} from './todo/todo.component';
-
+import {StartPageComponent} from './start-page/start-page.component';
 @NgModule({
-  declarations: [AppComponent, TodoComponent],
+  declarations: [AppComponent, TodoComponent, StartPageComponent],
   imports: [
     BrowserModule,
     AngularFireModule.initializeApp(environment.firebase),
-    AngularFireDatabaseModule
+    AngularFireDatabaseModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,41 +1,42 @@
 import {Component, OnInit} from '@angular/core';
-import {TodoService} from "./shared/todo.service";
+import {TodoService} from './shared/todo.service';
 import 'rxjs/Rx';
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
   styleUrls: ['./todo.component.css'],
-  providers : [TodoService]
+  providers: [TodoService],
 })
 export class TodoComponent implements OnInit {
   toDoListArray: any[];
   constructor(private toDoService: TodoService) {}
 
   ngOnInit() {
-    this.toDoService.getToDoList().snapshotChanges()
-      .subscribe(item=>{
+    this.toDoService
+      .getToDoList()
+      .snapshotChanges()
+      .subscribe(item => {
         this.toDoListArray = [];
-        item.forEach(element =>{
+        item.forEach(element => {
           let x = element.payload.toJSON();
-          x["$key"]=element.key;
+          x['$key'] = element.key;
           this.toDoListArray.push(x);
         });
-        this.toDoListArray.sort((a,b) =>{
+        this.toDoListArray.sort((a, b) => {
           return a.isChecked - b.isChecked;
         });
       });
   }
-  onAdd(itemTitle){
+  onAdd(itemTitle) {
     this.toDoService.addTitle(itemTitle.value);
     itemTitle.value = null;
   }
 
-  alterCheck($key: string, isChecked){
+  alterCheck($key: string, isChecked) {
     this.toDoService.checkOrUnCheckTitle($key, !isChecked);
   }
 
-  onDelete($key: string){
+  onDelete($key: string) {
     this.toDoService.removeTitle($key);
   }
-
 }
